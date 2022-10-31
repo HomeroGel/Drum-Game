a79ee535952eee859ce1bc2fa79f40ea645e2375
Homero Gelves <homerogelves@gmail.com>
2022-10-31 20:11:44 -0300

    agregue comentarios al codigo

--- functions.js
+++ functions.js
@@ -1,16 +1,16 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(audio)
-    if (!audio) return; //Detengo la funcion
+function playSound(e) { //Funcion que genera el sonido
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //en audio guardo el sonido, variando la tecla que se presiona.
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`) // en key guardo el div completo de la tecla que se presiona.
+    
+    if (!audio) return; //Si se presiona una tecla que no tiene un sonido asignado, detengo la funcion.
     audio.currentTime = 0; //lleva el sonido al inicio
     audio.play()
-    key.classList.add('playing')
+    key.classList.add('playing') // agrego la clase playing, que activa la tecla y le da estilos.
 }
 
 function removeTransition(e) {
     if (e.propertyName !== "transform") return;
-    this.classList.remove('playing')
+    this.classList.remove('playing') // luego de .8s borro la clase y la animacion de apretar, se borra.
 }
 
 const keys = document.querySelectorAll('.key'); //me traigo todos los div con clase .key y los guardo en la variable keys
